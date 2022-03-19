fdc88ff93afee20af322449f305bda7486c645ae
maemon <47819358+maemon4095@users.noreply.github.com>
2022-03-19 23:12:50 +0900

    program

--- Enumeration.Test/Program.cs
+++ Enumeration.Test/Program.cs
@@ -2,40 +2,7 @@
 
 var instance = C.M4(20, 2, "obj", "str", new byte[] { 1 });
 var b = B<int, string>.M0();
-
-switch (instance.Type)
-{
-    case C.Case.M0:
-    {
-        instance.M0();
-    }
-    break;
-    case C.Case.M1:
-    {
-        instance.M1(out var num);
-    }
-    break;
-    case C.Case.M2:
-    {
-        instance.M2(out var num, out var str);
-        Console.WriteLine($"num: {num}, str: {str}");
-    }
-    break;
-    case C.Case.M3:
-    {
-        instance.M3(out var obj, out var str, out var array);
-    }
-    break;
-    case C.Case.M4:
-    {
-        instance.M4(out var num, out var a, out var obj, out var str, out var array);
-        Console.WriteLine($"num: {num}, a:{a}, obj: {obj} str: {str}, array: {array}");
-    }
-    break;
-
-    default: break;
-}
-
+Console.WriteLine(b);
 Console.ReadLine();
 
 [Enumeration]
@@ -49,7 +16,7 @@ partial struct C
 }
 
 [Enumeration]
-partial class B<T0, T1>
+partial struct B<T0, T1>
     where T1 : class
 {
     public static partial B<T0, T1> M0();
